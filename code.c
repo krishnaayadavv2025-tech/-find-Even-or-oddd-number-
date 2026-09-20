9513c33722a3f57ecf98aacc5e45f0f4f679a40a
krishnaayadavv2025-tech <krishnaayadavv2025@gmail.com>
2026-09-20 13:59:38 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter number");
+    scanf("%d",&n);
+    if (n%2==0){
+    printf("Even ");
+    }
+    else{ printf("odd");
+    
+    }
+    return 0;
+}
